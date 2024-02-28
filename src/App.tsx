5ff50dbd05030214b30acd41cf2f6db1a61303df
EMAlbright <ethanmacalbright@gmail.com>
2024-02-28 01:51:16 -0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import homeMusic from './assets/music/home_background.wav';
 
 function App() {
+
+    const[isPlaying, setIsPlaying] = useState(false);
+    const audio = new Audio(homeMusic);
+
+    //play it
+    //no autoplay unless user interacts first
+    //workaround add button, so if there is a click it starts playing
+    //fix for better functionality later
+    const togglePlay = () => {
+      if(isPlaying){
+        audio.pause();
+      } else{
+        audio.play().catch(error => console.error("Error playing audio:", error));
+      }
+      setIsPlaying(!isPlaying);
+    };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className = "background-button" onClick={togglePlay}>
+          {isPlaying ? 'off' : 'play'}
+        </button>
       </header>
     </div>
   );
